2687e8628d66a164fc83e7b73b00ce178ffe8f4c
Kav1905 <kavyam19agr@gmail.com>
2024-08-10 10:12:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 from openpyxl import Workbook, load_workbook
-from MasterSheetMaker import *
+from MasterSheetMaker import getSubDict
+from MasterSheetMaker import t_pri, t_mid, t_sec
+from Constraints import lecPerWeek
 
 load_data = load_workbook("Data.xlsx")
 data = load_data["Master"]
+
+subDict = getSubDict()
+subCount = len(subDict)
 
 class Teacher():
     def __init__(self, t_code):
         self.t_code = t_code
 
 def teacher_list():
-    subDict = getSubDict()
-    print(Sub_dict)
-    t_pri = get_pri_teacher_codes()
-    t_mid = get_mid_teacher_codes()
-    t_sec = get_sec_teacher_codes()
+    print(subCount)
     print(t_pri, t_mid, t_sec, len(t_pri), len(t_mid), len(t_sec), sep = "\n")
+
+def lecPerWeekCheck():
+    pass
 
 teacher_list()
